2287bf13cffd3604b7130a27e749b70dad21f86b
jinweiteo <teojw94@gmail.com>
2022-04-29 08:38:07 +0800

    1. Added CustomAuthorizationFilter 2. Added authorisation filter in SecurityConfig 3. Added empty SecurityController

---
+++ src/main/java/com/access/accountmanagement/authentication/controller/SecurityController.java
@@ -0,0 +1,11 @@
+package com.access.accountmanagement.authentication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/security")
+public class SecurityController {
+
+
+}
--- src/main/java/com/access/accountmanagement/authentication/security/SecurityConfig.java
+++ src/main/java/com/access/accountmanagement/authentication/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.access.accountmanagement.authentication.security;
 
 import com.access.accountmanagement.authentication.filter.CustomAuthenticationFilter;
+import com.access.accountmanagement.authentication.filter.CustomAuthorizationFilter;
+import com.access.accountmanagement.common.constants.AccountConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,10 +32,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.authorizeRequests().antMatchers("/api/login").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/appuser/**").hasAnyAuthority(AccountConstants.ADMIN_ROLE);
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
